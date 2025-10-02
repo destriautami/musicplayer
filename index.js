3db17f204e965f1ac33fe1eacd088b024437469a
destriautami <destriaputriu@gmail.com>
2025-10-02 15:35:07 +0700

    commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 400,
+    height: 600,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.removeMenu(); // hilangkan menu bar default
+  win.loadFile("index.html"); // load file html utama
+}
+
+app.whenReady().then(createWindow);
+
+// Tutup app kalau semua window ditutup
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
